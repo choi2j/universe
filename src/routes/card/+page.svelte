cadc17806af4fe17aeceff4f449db8c343113522
scvif <jinbum2137@gmail.com>
2023-08-01 18:23:50 +0900

    svg files added

---
+++ src/routes/card/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import text from '../../assets/myIDCardText.svg';
+    import card from '../../assets/IDCardSample.svg';
+</script>
+
+
+
+<div id="card">
+    <img id="text" src={text} />
+    <img id="sample" src={card} />
+    <button>인증 카드 받기</button>
+</div>
+
+
+<style>
+    #card {
+        height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+    }
+
+    #text {
+        height: 64px;
+    }
+
+    #sample {
+        height: 600px;
+    }
+</style>
