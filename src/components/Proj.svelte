7b94f6833039a4a35e39ed978a88242163000a16
scvif <jinbum2137@gmail.com>
2023-08-28 19:39:21 +0900

    .

--- src/components/Proj.svelte
+++ src/components/Proj.svelte
@@ -8,30 +8,35 @@
 	import circ from "$lib/assets/props/whiteCircle.svg";
 	import leftArrow from "$lib/assets/leftarrow.svg";
 	import flag from "$lib/assets/project/flag.svg";
+	import icon1 from "$lib/assets/project/icon1.svg";
+	import icon2 from "$lib/assets/project/icon2.svg";
+	import icon3 from "$lib/assets/project/icon3.svg";
+	import icon4 from "$lib/assets/project/icon4.svg";
+	import icon5 from "$lib/assets/project/icon5.svg";
 	console.log(team);
 </script>
 
 <div class="proj">
-	<button>
-		<img src={leftArrow} alt="back">
-	</button>
-	<div class="info">
-		<p class="title text-big">{title}</p>
-		<p class="desc text-small">{desc}</p>
-		<div class="part-list">
-			{#each team as a}
-				<div class="part">
-					<img src={circ} alt="circle">
-					<p class="cont">{a.role} by {a.name}</p>
-				</div>
-			{/each}
-		</div>
 
+	<div class="f5417">
+		<div class="info">
+			<p class="title text-big">{title}</p>
+			<p class="desc text-small">{desc}</p>
+			<div class="f5411">
+				{#each team as a}
+					<div class="part">
+						<img src={circ} alt="circle">
+						<p class="cont">{a.role} by {a.name}</p>
+					</div>
+				{/each}
+			</div>
+		</div>
+		<div class="img">
+			<img {src} alt="" />
+		</div>
 	</div>
-	<div class="img">
-		<img {src} alt="" />
-	</div>
-	<div id="buttons">
+
+	<div class="f5426">
         <a href="만든 프로젝트 링크" class="btn">
             <div class="btn-bg" />
             <div class="btn-border" />
@@ -53,119 +58,7 @@
 		background: none;
 	}
 
-	.proj {
-		height: 100%;
-		width: 100%;
-		background: rgba(0, 0, 0, 0.60);
-		backdrop-filter: blur(100px);
-	}
-
-	.cont {
-		display: inline-block;
-		font-size: 1.5rem;
-	}
-
-	.part-list {
-		width: 600px;
-		display: grid;
-		grid-template-columns: 1fr 1fr;
-	}
-
-	.part {
-		display: flex;
-		flex-direction: row;
-		align-items:center
-	}
-
-	a {
-        position: fixed;
-        top: 100px;
-        left: 200px;
-    }
-
 	.text-big {
-        color: #FFF;
-        font-size: 48px;
-        font-style: normal;
-        font-weight: 700;
-        line-height: 100%; /* 48px */
-    }
-
-    .text-small {
-        color: #FFF;
-        font-size: 24px;
-        font-style: normal;
-        font-weight: 500;
-        line-height: normal;
-    }
-
-    .btn {
-		display: grid;
-		grid-template-columns: 1fr;
-		grid-template-rows: auto;
-	}
-
-	.btn-bg {
-		border-radius: 60rem;
-		grid-column: 1/2;
-		grid-row: 1/2;
-		z-index: 1;
-		background-color: #ffffff33;
-	}
-
-	.btn-border {
-		grid-column: 1/2;
-		grid-row: 1/2;
-		z-index: 2;
-		border-radius: 60rem;
-		border: 3px solid transparent;
-		background: linear-gradient(-45deg, #8c8aff, #db74ff00, #db74ff00, #db74ff) border-box;
-		-webkit-mask: linear-gradient(#fff 0 0) padding-box, linear-gradient(#fff 0 0);
-		-webkit-mask-composite: xor;
-		mask-composite: exclude;
-	}
-
-	.btn-text {
-		padding: 1rem 2.5rem;
-		grid-column: 1/2;
-		grid-row: 1/2;
-		white-space: nowrap;
-		color: white;
-		z-index: 2;
-		text-transform: uppercase;
-		font-size: 1.75rem;
-	}
-
-    #buttons {
-        display: flex;
-    }
-</style>
-
-
-
-
-
-
-<!-- <style>
-    #part {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        width: 600px;
-        height: 100px;
-    }
-
-    a {
-        position: fixed;
-        top: 100px;
-        left: 200px;
-    }
-
-    button {
-        background-color: #00000000;
-        border: none;
-    }
-
-    .text-big {
         color: #FFF;
         font-size: 48px;
         font-style: normal;
@@ -181,7 +74,61 @@
         line-height: normal;
     }
 
-    .btn {
+	.desc {
+		margin-top: 4.5rem;
+		margin-bottom: 6.5rem;
+		width: 37.25rem;
+	}
+
+	.proj {
+		height: 100%;
+		width: 100%;
+		background: rgba(0, 0, 0, 0.60);
+		backdrop-filter: blur(100px);
+	}
+
+	.cont {
+		display: inline-block;
+		font-size: 1.5rem;
+	}
+
+	.part {
+		display: flex;
+		flex-direction: row;
+		align-items:center;
+		gap: 0.75rem;
+	}
+
+	.img {
+		height: 31.25rem;
+		width: 31.25rem;
+	}
+
+	.f5411 {
+		width: 600px;
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 0.75rem;
+	}
+
+	.f5417 {
+		display: grid;
+		align-items: center;
+		grid-template-columns: auto 31.25rem;
+		margin: 18.5rem 12.5rem;
+	}
+
+    .f5426 {
+		position: fixed;
+        display: flex;
+		justify-content: center;
+		align-items: center;
+		top: 59.5em;
+		gap: 1rem;
+		width: 100%;
+    }
+
+	.btn {
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-template-rows: auto;
@@ -218,7 +165,5 @@
 		font-size: 1.75rem;
 	}
 
-    #buttons {
-        display: flex;
-    }
-</style> -->
+
+</style>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -3,26 +3,41 @@
 
     import leftArrow from "$lib/assets/leftarrow.svg";
     import rightArrow from "$lib/assets/rightarrow.svg";
-    import planetA from "$lib/assets/project/planetA_1.svg";
-    import planetB from "$lib/assets/project/planetB_1.svg";
-    import planetC from "$lib/assets/project/planetC_1.svg";
-    import planetD from "$lib/assets/project/planetD_1.svg";
-    import planetE from "$lib/assets/project/planetE_1.svg";
 	import Proj from "../../components/Proj.svelte";
 
     import data from "$lib/projData";
     $: state = 0;
     $: show = 0;
+
+    function toggleProj() {
+        // let target = document.querySelector('.comp');
+        // let collateralTarget = document.querySelector('.back');
+
+
+        let target = document.querySelector('#comp').style.display;
+        let collateralTarget = document.querySelector('#back').style.zIndex;
+
+        console.log(target);
+        console.log(collateralTarget);
+        target = 'none';
+        collateralTarget = 0;
+
+    }
 </script>
 
 <div id="planet">
+    <button id="back" on:click={() => {toggleProj()}}>
+        <img src={leftArrow} alt="back">
+    </button>
     <div id="togglePlanet">
         <div id="leftArrow"></div>
         <div id=""></div>
         <div id="rightArrow"></div>
     </div>
-
-    <Proj title={data[state].title} desc={data[state].desc} src={data[state].src} team={data[state].team} />
+    <div id="comp">
+        <Proj title={data[state].title} desc={data[state].desc} src={data[state].src} team={data[state].team} />
+    </div>
+    
 </div>
 
 <style>
@@ -31,4 +46,25 @@
         height: 100%;
         width: 100%;
     }
+
+    #comp {
+        height: 100%;
+        width: 100%;
+        display: flex;
+    }
+
+    #back {
+        
+        position: fixed;
+        left: 12.5rem;
+        top: 6.5rem;
+        z-index: 1;
+    }
+
+    button {
+		border: none;
+		background: none;
+	}
 </style>
+
+
